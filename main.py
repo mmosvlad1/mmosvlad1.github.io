4d462b4bb923652f19d242e0a0dd706cb008fdc6
mmosvlad1 <113801483+mmosvlad1@users.noreply.github.com>
2023-01-16 21:59:19 +0000

    v0.1

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import telebot
-bot = telebot.TeleBot("5985822950:AAH3rRW52PCmfg6ifnHKJTtQdyHXNSvYj4E")
-vlad_id = 541694991
-
+bot = telebot.TeleBot("5730532015:AAFuIck1d2VcGFBCMXehSGo_yLBhjzm2ZKA")
+user_identifier = [0]
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    bot.send_message(message.chat.id, "<b>Hello member of destruction</b>",parse_mode="html")
+    count = 0
+    bot.send_message(message.chat.id, "<b>Hello member of destruction</b>", parse_mode="html")
+    for i in range(0, len(user_identifier)):
+        if message.chat.id != user_identifier[i]:
+            count = count + 1
+    if count == len(user_identifier):
+        for i in range(0, len(user_identifier)):
+            if user_identifier[i] == 0:
+                user_identifier.append(0)
+                user_identifier[i] = message.chat.id
+                print(user_identifier[i])
+                break
 @bot.message_handler()
-def start_bot(message):
-    if message.chat.id == vlad_id:
-        bot.send_message(vlad_id, message.text,parse_mode="html")
+def transform_message(message):
+    for i in range(0, len(user_identifier)-1):
+        if message.chat.id == user_identifier[i]:
+            continue
+        else:
+            bot.send_message(user_identifier[i], message.text, parse_mode="html")
+
 bot.infinity_polling()
